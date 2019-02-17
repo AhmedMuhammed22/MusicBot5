07808b3ab147ab02e2c13571310fd6dd91e9ac58
AhmedMuhammed22 <44835732+AhmedMuhammed22@users.noreply.github.com>
2019-02-17 12:15:20 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,57 @@
 import discord
-import discord.ext.commands
 from discord.ext import commands
+from discord.ext.commands import Bot
 import asyncio
-import time
+import random
+import requests
+import os
 
-Client = discord.Client()
-client = commands.Bot(command_prefix = "!")
+client = commands.Bot(command_prefix=":")
+player_dict = dict()
 
 
 @client.event
 async def on_ready():
-    print ("Bot is online and connected to Discord")
-    
-@client.event
-async def on_message(message):
-    if message.content.startswith('!PING'):
-        userID = message.author.id
-        await client.send_message(message.channel, "<@%s> Pong!" % (userID))
-        if message.content.upper().startswith('!SAY'):
-            if message.author.id == "307820755440107520":
-             args = message.content.split(" ")
-             await client.send_message(message.channel, "%s" % (" ".join(args[1:])))
-             else:
-                 await client.send_message(message.channel, "You do not have permission")
-
- 
+    print("Bot ist bereit")
 
 
+@client.command(pass_context=True)
+async def play (ctx, url):
+    channel = ctx.message.author.voice_channel
+    await client.join_voice_channel(channel)
+    server = ctx.message.server
+    voice = client.voice_client_in(server)
+    player = await voice.create_ytdl_player(url)
+    player_dict[server.id] = player
+    await client.send_message(ctx.message.channel, "Playing `%s` now" % player.title)
+    player.start()
 
-client.run("BOT_TOKEN")
 
+@client.command(pass_context=True)
+async def stop(ctx):
+    server = ctx.message.server
+    player = player_dict[server.id]
+    player.stop()
+    await client.send_message(ctx.message.channel, "Stopped `%s`" % player.title)
+    del player_dict[server.id]
+
+
+@client.command(pass_context=True)
+async def pause(ctx):
+    server = ctx.message.server
+    player = player_dict[server.id]
+    player.pause()
+    await client.send_message(ctx.message.channel, "Paused `%s`" % player.title)
+
+
+@client.command(pass_context=True)
+async def resume(ctx):
+    server = ctx.message.server
+    player = player_dict[server.id]
+    player.resume()
+    await client.send_message(ctx.message.channel, "Resumed `%s`" % player.title)
+
+
+
+
+client.run(str(os.environ.get('BOT_TOKEN')))
